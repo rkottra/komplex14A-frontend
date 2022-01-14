da9eb302c48a31f8b38f864b802e9584f4bac0d9
Kottra Richárd <kottra.richard@jedlik.eu>
2022-01-14 13:25:02 +0100

    pilota komponens, pilota szervíz

---
+++ src/app/pilota/pilota.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pilota',
+  templateUrl: './pilota.component.html',
+  styleUrls: ['./pilota.component.css']
+})
+export class PilotaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
